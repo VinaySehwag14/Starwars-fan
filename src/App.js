f51b8f5e1614bce748d748d76a76908b3482afce
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-01-23 12:20:20 +0530

    ERROR OF DATA FETCHING LOADING CORRECTED WITH STATE

--- src/App.js
+++ src/App.js
@@ -5,32 +5,35 @@ import { useState } from "react";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMoviesHandler = () => {
-    fetch(" https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
+  const fetchMoviesHandler = async () => {
+    setIsLoading(true);
+    const response = await fetch(" https://swapi.dev/api/films/");
+    const data = await response.json();
 
-        setMovies(transformedMovies);
-      });
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+
+    setMovies(transformedMovies);
+    setIsLoading(false);
   };
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No movies found !!</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
